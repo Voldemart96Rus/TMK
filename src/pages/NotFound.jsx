e17bf30a591de8f4ae051ede7ba442eb0e9a933c
OlgaCheraneva <e3250@yandex.ru>
2020-06-14 21:51:05 +0500

    fixed page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,6 +7,13 @@ const NotFound = () => {
         <section className="container-lg content">
             <div className="row">
                 <Catalog />
+                <div class="col-md-9">
+                    <section class="delivery">
+                        <h3 class="block__header">
+                            Запрашиваемая Вами страница не существует!
+                        </h3>
+                    </section>
+                </div>
             </div>
         </section>
     );

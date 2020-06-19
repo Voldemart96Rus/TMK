3aac3bc06960dda62a963b47a8787d596fd9f40a
Olga <cheraneva197@gmail.com>
2020-06-19 19:26:53 +0500

    Main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 import CatalogAside from '../components/layout/CatalogAside';
+import CompanyDescription from '../components/CompanyDescription';
 
 function Home() {
     return (
@@ -12,48 +13,79 @@ function Home() {
                 <div className="col col-md-7 col-lg-9">
                     <section className="about">
                         <h3 className="block__header">О компании</h3>
-                        <Carousel>
+                        <Carousel indicators={false}>
                             <Carousel.Item>
                                 <img
-                                    className="d-block w-100 carousel__image of_contain pb-5"
+                                    className="d-block w-100 carousel__image of_contain carousel__small-image"
                                     src="/img/carousel-1.png"
                                     alt=""
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
                                 <img
-                                    className="d-block w-100 carousel__image pb-5"
+                                    className="d-block w-100 carousel__image"
                                     src="/img/carousel-2.jfif"
                                     alt=""
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
                                 <img
-                                    className="d-block w-100 carousel__image pb-5"
+                                    className="d-block w-100 carousel__image"
                                     src="/img/carousel-3.jfif"
                                     alt=""
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
                                 <img
-                                    className="d-block w-100 carousel__image pb-5"
+                                    className="d-block w-100 carousel__image"
                                     src="/img/carousel-4.jpg"
                                     alt=""
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
                                 <img
-                                    className="d-block w-100 carousel__image pb-5"
+                                    className="d-block w-100 carousel__image"
                                     src="/img/carousel-5.jpg"
                                     alt=""
                                 />
                             </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100 carousel__image"
+                                    src="/img/carousel-6.jpg"
+                                    alt=""
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100 carousel__image"
+                                    src="/img/carousel-7.jfif"
+                                    alt=""
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100 carousel__image"
+                                    src="/img/bg.jpg"
+                                    alt=""
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100 carousel__image"
+                                    src="/img/tmk-prod-lg.jpg"
+                                    alt=""
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="d-block w-100 carousel__image"
+                                    src="/img/plant.jpg"
+                                    alt=""
+                                />
+                            </Carousel.Item>
                         </Carousel>
-                        {/* <img
-                            className="block__image"
-                            src="img/home.webp"
-                            alt="Трубы"
-                        /> */}
+                        <CompanyDescription />
                     </section>
                 </div>
             </div>

06aa4df21e65c12e06e74a5b16298db2a85f1f1a
OlgaCheraneva <e3250@yandex.ru>
2020-06-19 21:22:56 +0500

    Добил товары и отключил у каталога категории

--- src/components/initialState.js
+++ src/components/initialState.js
@@ -48,7 +48,7 @@ const initialState = {
         {
             id: 7,
             title: 'Труба ПНД',
-            img: 'img/catalog/Труба ПНД/ПНД_для_канализации.png',
+            img: '/img/catalog/Труба ПНД/ПНД_для_канализации.png',
             URL: '/category/Truba_PND',
         },
     ],
@@ -60,111 +60,137 @@ const initialState = {
                     title: 'Опоры скользящие СПО',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опора_скользящая_СПО.png',
+                    URL: '/product/Opory_skolzyashchiye_SPO',
                 },
                 {
                     title: 'Опоры скользящие хомутовые 313.ТС-008.011',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опора_скользящая_хомутовая_313.ТС-008.011.png',
+                    URL:
+                        '/product/Opory_skolzyashchiye_khomutovyye_313.TS-008.011',
                 },
                 {
                     title: 'Опоры скользящие хомутовые ФСО 1',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_скользящие_ФСО_1.png',
+                    URL: '/product/Opory_skolzyashchiye_khomutovyye_FSO_1',
                 },
                 {
                     title: 'Опоры скользящие хомутовые ФСО 2',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_скользящие_ФСО_2.png',
+                    URL: '/product/Opory_skolzyashchiye_khomutovyye_FSO_2',
                 },
                 {
                     title: 'Опоры скользящие приварные ОПБ1',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_ОПБ_1.png',
+                    URL: '/product/Opory_skolzyashchiye_privarnyye_OPB1',
                 },
                 {
                     title: 'Опоры скользящие приварные ОПБ2',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_ОПБ_2.png',
+                    URL: '/product/Opory_skolzyashchiye_privarnyye_OPB2',
                 },
                 {
                     title: 'Опоры скользящие приварные ОПП1',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_ОПП_1.png',
+                    URL: '/product/Opory_skolzyashchiye_privarnyye_OPP1',
                 },
                 {
                     title: 'Опоры скользящие ОПП2',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_ОПП_2.png',
+                    URL: '/product/Opory_skolzyashchiye_OPP2',
                 },
                 {
                     title: 'Опоры скользящие хомутовые ОПХ1',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_ОПХ_1.png',
+                    URL: '/product/Opory_skolzyashchiye_khomutovyye_OPKH1',
                 },
                 {
                     title: 'Опоры скользящие хомутовые ОПХ2',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_ОПХ_2.png',
+                    URL: '/product/Opory_skolzyashchiye_khomutovyye_OPKH2',
                 },
                 {
                     title: 'Опоры скользящие хомутовые ОПХ3',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_ОПХ_3.png',
+                    URL: '/product/Opory_skolzyashchiye_khomutovyye_OPKH3',
                 },
                 {
                     title: 'Опоры скользящие Т13',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_Т13.png',
+                    URL: '/product/Opory_skolzyashchiye_T13',
                 },
                 {
                     title: 'Опоры скользящие Т14',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_Т14.png',
+                    URL: '/product/Opory_skolzyashchiye_T14',
                 },
                 {
                     title: 'Опоры скользящие ТС-623',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_ТС-623.png',
+                    URL: '/product/Opory_skolzyashchiye_TS-623',
                 },
                 {
                     title: 'Опоры скользящие ТС-624',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_ТС-624.png',
+                    URL: '/product/Opory_skolzyashchiye_TS-624',
                 },
                 {
                     title: 'Опоры скользящие ТС-625',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_ТС-625.png',
+                    URL: '/product/Opory_skolzyashchiye_TS-625',
                 },
                 {
                     title: 'Опоры скользящие ТС-631',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_подвижные_ТС-631.png',
+                    URL: '/product/Opory_skolzyashchiye_TS-631',
                 },
                 {
                     title: 'Опоры неподвижные хомутовые Т3',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_неподвижные_Т3.png',
+                    URL: '/product/Opory_nepodvizhnyye_khomutovyye_T3',
                 },
                 {
                     title: 'Опоры неподвижные лобовые 2-х упорные Т4',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_неподвижные_Т4.png',
+                    URL:
+                        '/product/Opory_nepodvizhnyye_lobovyye_2-kh_upornyye_T4',
                 },
                 {
                     title: 'Опоры неподвижные лобовая 2-х упорная ТС-660',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_неподвижные_Т4.png',
+                    URL:
+                        '/product/Opory_nepodvizhnyye_lobovaya_2-kh_upornaya_TS-660',
                 },
                 {
                     title: 'Опоры неподвижные хомутовые корпусные ТС-670',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_неподвижные_ТС-670.png',
+                    URL:
+                        '/product/Opory_nepodvizhnyye_khomutovyye_korpusnyye_TS-670',
                 },
                 {
                     title: 'Опоры неподвижные хомутовые ТС-659',
                     img:
                         '/img/catalog/Опоры трубопроводов/Опоры_неподвижные_ТС659.png',
+                    URL: '/product/Opory_nepodvizhnyye_khomutovyye_TS-659',
                 },
             ],
         },
@@ -172,60 +198,244 @@ const initialState = {
             title: 'Трубы и опоры в изоляции',
             items: [
                 {
-                    id: 0,
-                    title: '',
-                    img: 'img/catalog/Трубы И Опоры В Изоляции/',
-                    URL: '/category/Truba_PND',
+                    title: 'Труба в ППУ изоляции полиэтиленовая оболочка',
+                    img:
+                        '/img/catalog/Трубы И Опоры В Изоляции/изоляция_труб_ППУ_полиэтилен.jpg',
+                    URL:
+                        '/product/Truba_v_PPU_izolyatsii_polietilenovaya_obolochka',
                 },
                 {
-                    id: 1,
-                    title: '',
-                    img: 'img/catalog/Трубы И Опоры В Изоляции/',
-                    URL: '/category/Truba_PND',
+                    title: 'Труба в ППУ изоляции оцинкованная оболочка',
+                    img:
+                        '/img/catalog/Трубы И Опоры В Изоляции/изоляция_труб_ППУ_оцинк.jpg',
+                    URL:
+                        '/product/Truba_v_PPU_izolyatsii_otsinkovannaya_obolochka',
                 },
                 {
-                    id: 2,
-                    title: '',
-                    img: 'img/catalog/Трубы И Опоры В Изоляции/',
-                    URL: '/category/Truba_PND',
+                    title: 'Труба в ППМИ изоляции',
+                    img:
+                        '/img/catalog/Трубы И Опоры В Изоляции/изоляция_труб_ППМИ.jpg',
+                    URL: '/product/Truba_v_PPMI_izolyatsii',
                 },
                 {
-                    id: 3,
-                    title: '',
-                    img: 'img/catalog/Трубы И Опоры В Изоляции/',
-                    URL: '/category/Truba_PND',
+                    title: 'Опора скользящая хомутовая ППУ',
+                    img:
+                        '/img/catalog/Трубы И Опоры В Изоляции/Опора_скользящая_хомутовая.jpg',
+                    URL: '/product/Opora_skolzyashchaya_khomutovaya_PPU',
                 },
                 {
-                    id: 4,
-                    title: '',
-                    img: 'img/catalog/Трубы И Опоры В Изоляции/',
-                    URL: '/category/Truba_PND',
+                    title: 'Опора неподвижная в ППУ изоляции полиэтиленовая',
+                    img:
+                        '/img/catalog/Трубы И Опоры В Изоляции/Опора_неподвижная_в_ППУ_изоляции_полиэтиленовая_оболочка.jpg',
+                    URL:
+                        '/product/Opora_nepodvizhnaja_v_ppu_izoljatsii_polietilenovaja_obolochka',
                 },
                 {
-                    id: 5,
-                    title: '',
-                    img: 'img/catalog/Трубы И Опоры В Изоляции/',
-                    URL: '/category/Truba_PND',
+                    title:
+                        'Опора неподвижная в ППУ изоляции оцинкованная оболочка',
+                    img:
+                        '/img/catalog/Трубы И Опоры В Изоляции/Опора_неподвижная_в_ППУ_изоляции_оцинкованная_оболочка.jpg',
+                    URL:
+                        '/product/Opora_nepodvizhnaja_v_ppu_izoljatsii_otsinkovannaja_obolochka',
                 },
             ],
         },
         Otvody_v_izolyatsii: {
             title: 'Отводы в изоляции',
+            items: [
+                {
+                    title: 'Отвод в ППУ изоляции полиэтиленовая оболочка',
+                    img:
+                        '/img/catalog/Отводы В Изоляции/отвод_в_ППУ_изоляции_полиэтилен.jpg',
+                    URL:
+                        '/product/Otvod_v_PPU_izolyatsii_polietilenovaya_obolochka',
+                },
+                {
+                    title: 'Отвод в ППУ изоляции оцинкованная оболочка',
+                    img:
+                        '/img/catalog/Отводы В Изоляции/отвод_в_ППУ_изоляции_оцинк.jpg',
+                    URL:
+                        '/product/Otvod_v_PPU_izolyatsii_otsinkovannaya_obolochka',
+                },
+                {
+                    title: 'Отвод в ППУ изоляции стеклопластик',
+                    img:
+                        '/img/catalog/Отводы В Изоляции/отвод_в_ППУ_изоляции_стеклопластик.jpg',
+                    URL: '/product/Otvod_v_PPU_izolyatsii_stekloplastik',
+                },
+                {
+                    title: 'Отвод в ППУ изоляции фольга Армофол',
+                    img:
+                        '/img/catalog/Отводы В Изоляции/Отвод_в_ППУ_изоляции_фольга_Армофол.jpg',
+                    URL: '/product/Otvod_v_PPU_izolyatsii_folga_Armofol',
+                },
+                {
+                    title: 'Отвод в ППУ изоляции фольга кашированная',
+                    img:
+                        '/img/catalog/Отводы В Изоляции/Отвод_в_ППУ_изоляции_фольга_кашированная.jpg',
+                    URL: '/product/Otvod_v_PPU_izolyatsii_folga_kashirovannaya',
+                },
+                {
+                    title: 'Кожух из оцинкованной стали на отвод',
+                    img:
+                        '/img/catalog/Отводы В Изоляции/Кожух_из_оцинкованной_стали_на_отвод.jpg',
+                    URL: '/product/Kozhukh_iz_otsinkovannoy_stali_na_otvod',
+                },
+            ],
         },
         skorlupa_ppu: {
             title: 'Скорлупа ППУ',
+            items: [
+                {
+                    title: 'Скорлупа ППУ без покрытия',
+                    img: '/img/catalog/Скорлупа ППУ/Скорлупа_ППУ.jpg',
+                    URL: '/product/Skorlupa_PPU_bez_pokrytiya',
+                },
+                {
+                    title: 'Скорлупа ППУ стеклопластик',
+                    img:
+                        '/img/catalog/Скорлупа ППУ/Скорлупа_ППУ_стеклопластик.jpg',
+                    URL: '/product/Skorlupa_PPU_stekloplastik',
+                },
+                {
+                    title: 'Скорлупа ППУ фольга Армофол',
+                    img:
+                        '/img/catalog/Скорлупа ППУ/Скорлупа_ППУ_фольга_Армофол.jpg',
+                    URL: '/product/Skorlupa_PPU_folga_Armofol',
+                },
+                {
+                    title: 'Скорлупа ППУ фольга кашированная',
+                    img:
+                        '/img/catalog/Скорлупа ППУ/Скорлупа_ППУ_фольга_кашированная.jpg',
+                    URL: '/product/Skorlupa_PPU_folga_kashirovannaya',
+                },
+                {
+                    title: 'Кожух из оцинкованной стали на скорлупу',
+                    img:
+                        '/img/catalog/Скорлупа ППУ/Кожух_из_оцинкованной_стали_на_скорлупу.jpg',
+                    URL: '/product/Kozhukh_iz_otsinkovannoy_stali_na_skorlupu',
+                },
+            ],
         },
         Otvod_mineralovatnyy: {
             title: 'Отвод минераловатный',
+            items: [
+                {
+                    title: 'Отвод минераловатный без покрытия',
+                    img:
+                        '/img/catalog/Отвод Минераловатный/Отвод_минераловатный.jpg',
+                    URL: '/product/Otvod_mineralovatnyy_bez_pokrytiya',
+                },
+                {
+                    title: 'Отвод минераловатный в оцинкованной оболочке',
+                    img:
+                        '/img/catalog/Отвод Минераловатный/Отвод_минераловатный_в_оцинкованной_оболочке.jpg',
+                    URL:
+                        '/product/Otvod_mineralovatnyy_v_otsinkovannoy_obolochke',
+                },
+                {
+                    title: 'Отвод минераловатный кашированный фольгой',
+                    img:
+                        '/img/catalog/Отвод Минераловатный/Отвод_минераловатный_кашированный_фольгой.jpg',
+                    URL: '/product/Otvod_mineralovatnyy_kashirovannyy_folgoy',
+                },
+                {
+                    title: 'Отвод минераловатный с защитным покрытием Outside',
+                    img:
+                        '/img/catalog/Отвод Минераловатный/Отвод_минераловатный_с_защитным_покрытием_Outside.jpg',
+                    URL:
+                        '/product/Otvod_mineralovatnyy_s_zashchitnym_pokrytiyem_Outside',
+                },
+            ],
         },
         Troynik_mineralovatnyy: {
             title: 'Тройник минераловатный',
+            items: [
+                {
+                    title: 'Тройник минераловатный без покрытия',
+                    img:
+                        '/img/catalog/Тройник Минераловатный/Тройник_минераловатный_без_покрытия.jpg',
+                    URL: '/product/Troynik_mineralovatnyy_bez_pokrytiya',
+                },
+                {
+                    title: 'Тройник минераловатный в оцинкованной оболочке',
+                    img:
+                        '/img/catalog/Тройник Минераловатный/Тройник_минераловатный_в_оцинкованной_оболочке.jpg',
+                    URL:
+                        '/product/Troynik_mineralovatnyy_v_otsinkovannoy_obolochke',
+                },
+                {
+                    title: 'Тройник минераловатный кашированный фольгой',
+                    img:
+                        '/img/catalog/Тройник Минераловатный/Тройник_минераловатный_кашированный_фольгой.jpg',
+                    URL: '/product/Troynik_mineralovatnyy_kashirovannyy_folgoy',
+                },
+                {
+                    title:
+                        'Тройник минераловатный с защитным покрытием Outside',
+                    img:
+                        '/img/catalog/Тройник Минераловатный/Тройник_минераловатный_с_защитным_покрытием_Outside.jpg',
+                    URL:
+                        '/product/Troynik_mineralovatnyy_s_zashchitnym_pokrytiyem_Outside',
+                },
+            ],
         },
         Tsilindr_mineralovatnyy: {
             title: 'Цилиндр минераловатный',
+            items: [
+                {
+                    title: 'Цилиндр минераловатный без покрытия',
+                    img:
+                        '/img/catalog/Цилиндр Минераловатный/Цилиндр_минераловатный.jpg',
+                    URL: '/product/Tsilindr_mineralovatnyy_bez_pokrytiya',
+                },
+                {
+                    title: 'Цилиндр минераловатный в оцинкованной оболочке',
+                    img: '/img/catalog/Цилиндр Минераловатный/350х350.png',
+                    URL:
+                        '/product/Tsilindr_mineralovatnyy_v_otsinkovannoy_obolochke',
+                },
+                {
+                    title: 'Цилиндр минераловатный кашированный фольгой',
+                    img:
+                        '/img/catalog/Цилиндр Минераловатный/Цилиндр_минераловатный_кашированный_фольгой.jpg',
+                    URL:
+                        '/product/Tsilindr_mineralovatnyy_kashirovannyy_folgoy',
+                },
+                {
+                    title:
+                        'Цилиндр минераловатный с защитным покрытием Outside',
+                    img:
+                        '/img/catalog/Цилиндр Минераловатный/Цилиндр_минераловатный_с_защитным_покрытием_Outside.jpg',
+                    URL:
+                        '/product/Tsilindr_mineralovatnyy_s_zashchitnym_pokrytiyem_Outside',
+                },
+            ],
         },
         Truba_PND: {
             title: 'Труба ПНД',
+            items: [
+                {
+                    title: 'Труба ПНД для канализации',
+                    img: '/img/catalog/Труба ПНД/ПНД_для_канализации.png',
+                    URL: '/product/Truba_PND_dlya_kanalizatsii',
+                },
+                {
+                    title: 'Труба ПНД в бухтах',
+                    img: '/img/catalog/Труба ПНД/ПНД_Бухта.png',
+                    URL: '/product/Truba_PND_v_bukhtakh',
+                },
+                {
+                    title: 'Труба ПНД для газа',
+                    img: '/img/catalog/Труба ПНД/ПНД_на_газ.png',
+                    URL: '/product/Truba_PND_dlya_gaza',
+                },
+                {
+                    title: 'Труба ПНД для воды',
+                    img: '/img/catalog/Труба ПНД/ПНД_для_воды.png',
+                    URL: '/product/Truba_PND_dlya_vody',
+                },
+            ],
         },
     },
 };
